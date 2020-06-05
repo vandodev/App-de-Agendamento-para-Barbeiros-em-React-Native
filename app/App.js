51fb1be76ba35e2f6a7a8356d5ca8d3bba425e7f
Evandro oliveira <vandoaparecido@hotmail.com>
2020-06-05 01:01:36 -0300

    Criação da context API

--- app/App.js
+++ app/App.js
@@ -2,10 +2,15 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import MainStack from './src/Stacks/MainStack.js';
 
+import userContextProvider from './src/context/UserContext';
+
 export default () => {
   return(
-    <NavigationContainer>
-        <MainStack />
-    </NavigationContainer>
+    <userContextProvider>
+      <NavigationContainer>
+          <MainStack />
+      </NavigationContainer>
+    </userContextProvider>
+    
   );
 }
