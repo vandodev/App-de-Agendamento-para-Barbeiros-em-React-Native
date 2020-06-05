6d2b094a4e4e3bf510c3a02f7d60d189cc422c3e
Evandro oliveira <vandoaparecido@hotmail.com>
2020-06-05 00:42:14 -0300

    Criando página de preload

--- app/src/Screens/Preload/index.js
+++ app/src/Screens/Preload/index.js
@@ -1,11 +1,31 @@
-import React from 'react';
-import {Text} from 'react-native';
-import {Container} from './styles';
+import React, {useEffect} from 'react';
+import {Container,LoadingIcon} from './styles';
+import AsyncStorage from '@react-native-community/async-storage';
+import {useNavigation} from '@react-navigation/native';
+import BarberLogo from '../../assets/barber.svg';
 
 export default () => {
+
+    const navigation = useNavigation();
+
+useEffect(() =>{
+    const checkToken = async () => {
+        const token = await AsyncStorage.getItem('token');
+
+        if(token !== null){
+
+        }else{
+            navigation.navigate('SignIn');
+        }
+    }
+    checkToken();
+    
+},[])
+
     return(
         <Container>
-            <Text>Preload</Text>
+            <BarberLogo whidth="100%" height="160"/>
+            <LoadingIcon size="large" color="#FFFFFF" />
         </Container>
     );
 }
